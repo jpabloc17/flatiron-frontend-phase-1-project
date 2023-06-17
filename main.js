afff17b5fb5473acfb002c98d34a3951d13dd9c7
Pablo Castro <pablocas-tro1@outlook.com>
2023-06-16 22:18:11 -0400

    add pseudo code for renderMealCategories

--- main.js
+++ main.js
@@ -21,7 +21,28 @@ const getMealCategories = async () => {
 // render meal categories data to dom
 const renderMealCategories = (mealCategoriesObj) => {
   console.log("renderMealCategories");
-  console.table(mealCategoriesObj);
+  console.table(mealCategoriesArray);
+  
+  // find and select a UL with meal categories ID dom element to append my data into
+
+  // for each element in our meal categoties array
+    // create a wrapping element maybe <article> class of card
+    //append the article.card to DOM
+    //display the category name, image, and description
+    // image
+      // crete an img element
+      // set img src to category thumbnail url
+      // give class of category-thumbnail
+      // give it a mobile friendly max width of like 300px
+      // appendChild the img to our card
+    // name
+    // create an h4 element
+      // set h4 element textContent to be meal category name
+      // appendChild the h4 to our card
+    // description
+    // create an p element
+      // set p element textContent to be meal category description
+      // appendChild the p to our card
 };
 
 // function to call when form input is given focus
