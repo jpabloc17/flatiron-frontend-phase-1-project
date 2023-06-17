794abe8c4b30637bb771a7252e8566f9ac15ca85
Pablo Castro <pablocas-tro1@outlook.com>
2023-06-16 21:36:32 -0400

    fetch meal categories data

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 const searchTermsInput = document.getElementById("search-terms");
+const getMealCategories = async () => {
+  const mealCategoriesApiURL =
+    "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+  try {
+    const response = await fetch(mealCategoriesApiURL);
+    const data = response.json();
+    console.log(" data: ", data);
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong, try again later");
+  }
+};
+
 const handleFormInputFocus = async () => {
   console.log("focus");
 
